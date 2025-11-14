6067a4802543406f7d15e335419d917cd1d157b4
yunkai huang <sky1411898191@gmail.com>
2025-11-14 15:28:31 +1100

    Add the logic for the div to change color and the dynamic grid number, the core function is finished

--- scirpt.js
+++ scirpt.js
@@ -1,20 +1,47 @@
-let gridNum = 16;
+const gridNum = document.querySelector("#inputSize");
+let painting = false;
 
 // div that is the single grid
 
+const changeGrid = document.querySelector("#changeGrid");
+changeGrid.addEventListener("click", ()=>{
+    containerDiv.innerHTML = '';
+    let num = prompt("What's your desire grid number?");
+
+    createGrid(num);
+});
+
+const containerDiv = document.querySelector("#container");
 
 
-const containerDiv = document.querySelector("#container")
-
-
-function createGrid(){
-    for (let index = 0; index < 256; index++) {
+function createGrid(gridNum){
+    for (let index = 0; index < gridNum*gridNum; index++) {
         const div = document.createElement("div");
         div.classList.add("square");
+        div.style.height = `${960/gridNum -2}px`;
+        console.log((960/gridNum -2));
+        div.style.width = `${960/gridNum -2}px`;
         containerDiv.appendChild(div);
+        div.addEventListener("mousedown",()=>{
+            painting = true;
+            addColorClass(div);
+        });
+        containerDiv.addEventListener("mouseup", ()=>{
+            painting = false;
+        });
+        div.addEventListener("mouseover",()=>{
+            if (painting){
+                addColorClass(div);
+            }
+        });
     }
     
 }
 
-createGrid();
+function addColorClass(selectClass){
+    selectClass.classList.add("colored");
+}
+createGrid(16);
+
+
 
