c8e1cf0b3bf6553dff1b9d8a44d815feeedddf5a
yunkai huang <sky1411898191@gmail.com>
2025-11-14 13:53:01 +1100

    Create js/css/html files, add a container and div for the grid

---
+++ scirpt.js
@@ -0,0 +1,9 @@
+// div that is the single grid
+const div = document.createElement("div");
+div.classList.add("square")
+
+
+const containerDiv = document.querySelector("#container")
+containerDiv.appendChild(div);
+
+
