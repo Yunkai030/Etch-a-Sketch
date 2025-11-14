54a439cbc8fcae2cc8bf6ae7de63f55f791e6450
yunkai huang <sky1411898191@gmail.com>
2025-11-14 16:13:32 +1100

    Change the overall size to 800x800, then optimize the hover effect to make it smoother.

--- scirpt.js
+++ scirpt.js
@@ -1,13 +1,17 @@
 const gridNum = document.querySelector("#inputSize");
 let painting = false;
+let lastGridNumber = 16;
+
+
+const buttonClear = document.querySelector("#clear");
+buttonClear.addEventListener("click",clearGrid);
 
 // div that is the single grid
-
 const changeGrid = document.querySelector("#changeGrid");
 changeGrid.addEventListener("click", ()=>{
     containerDiv.innerHTML = '';
     let num = prompt("What's your desire grid number?");
-
+    lastGridNumber = num;
     createGrid(num);
 });
 
@@ -18,29 +22,44 @@ function createGrid(gridNum){
     for (let index = 0; index < gridNum*gridNum; index++) {
         const div = document.createElement("div");
         div.classList.add("square");
-        div.style.height = `${960/gridNum -2}px`;
+        div.style.height = `${800/gridNum -2}px`;
         console.log((960/gridNum -2));
-        div.style.width = `${960/gridNum -2}px`;
+        div.style.width = `${800/gridNum -2}px`;
         containerDiv.appendChild(div);
-        div.addEventListener("mousedown",()=>{
+        div.addEventListener("mousedown",(e)=>{
+             e.preventDefault();
             painting = true;
             addColorClass(div);
         });
-        containerDiv.addEventListener("mouseup", ()=>{
-            painting = false;
-        });
-        div.addEventListener("mouseover",()=>{
+         div.addEventListener("mouseover",()=>{
             if (painting){
                 addColorClass(div);
             }
         });
+        
+        
     }
-    
+   
+
+}
+
+
+
+document.addEventListener("mouseup", ()=>{
+            painting = false;
+        });
+
+
+function clearGrid() {
+    containerDiv.innerHTML = "";
+    createGrid(lastGridNumber);
 }
 
 function addColorClass(selectClass){
     selectClass.classList.add("colored");
 }
+
+
 createGrid(16);
 
 
